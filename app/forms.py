15878dae48907ced35c08e306d6338f27661e852
Karenkoech <karenkoech3@gmail.com>
2022-05-11 04:55:50 -0400

    update the form files

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_login import current_user
 from flask_wtf.file import FileField, FileAllowed
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField,TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 from app.models import User
@@ -61,4 +61,8 @@ class RequestResetForm(FlaskForm):
             raise ValidationError('There is no account with that email. You must register first.')
 
 
-         
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
+
